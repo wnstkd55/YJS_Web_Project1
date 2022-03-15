9153d9e35e089c69ebb883ec56c32ebbea8cec37
JoonSang Yoo <js960913@naver.com>
2022-03-15 13:04:32 +0900

    First Commit 2022.03.14

---
+++ javascript/script1.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function(){
+    $('#toggle').click(function() {
+        $(this).toggleClass('is-active');
+        $('#navbarCollapse').toggleClass('is-active');
+      });
+})
