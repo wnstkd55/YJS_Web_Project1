4fa2f834480a0d86e8e4d8644060c4c35ba70ae8
JoonSang Yoo <js960913@naver.com>
2022-03-21 22:45:21 +0900

    [update] - 2022.03.21_메인페이지 로고 수정, 작품 메뉴 수정 [create] _ 602공방 작품 상세페이지 및 공방소개페이지 생성

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,7 @@ jQuery(document).ready(function(){
       });
       
 })
+// 서브메뉴 이벤트
 $(document).ready(function(){
   $(".menu>a").click(function(){
    var submenu = $(this).next("#Navbar-menu-submenu");
@@ -15,3 +16,15 @@ $(document).ready(function(){
      }
      });
      });
+//맨위로 가기 버튼
+     $(document).ready(function() {
+      $('#movetopbt').bind('click', function() {
+        $('html, body').animate({scrollTop: '0'}, 680);
+      });
+      // 애니메이션 효과로 자연스럽게 이동됨, 0.68초
+    
+      $('#movetopbt').on('click', function() {
+        $('html').scrollTop('0');
+      });
+      // 애니메이션 효과없이 즉시 해당 위치 0 지점으로 이동함
+    });
