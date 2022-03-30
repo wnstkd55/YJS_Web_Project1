3a660dcdfff83329d9c3a1c057d8203746a0e121
JoonSang Yoo <js960913@naver.com>
2022-03-31 00:11:26 +0900

    [update] - 2022.03.31_ 최종수정

--- javascript/script.js
+++ javascript/script.js
@@ -56,3 +56,20 @@ $(document).ready(function(){
       maxHeight: 1000
   });
 });
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+      $('#back-to-top').fadeIn();
+  } else {
+      $('#back-to-top').fadeOut();
+  }
+});
+
+
+$('#back-to-top').click(function () {
+  $('#back-to-top').tooltip('hide');
+  $('body,html').animate({
+      scrollTop: 0
+  }, 800);
+  return false;
+});
+$('#back-to-top').tooltip('show');
